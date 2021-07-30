c741b70ed0ecd9315b33e0abeab63dffde7f3a2b
Yamini-byte <56766286+Yamini-byte@users.noreply.github.com>
2021-07-30 14:24:47 +0530

    Add files via upload

---
+++ strings/assignment/formTheBiggestNumber.java
@@ -0,0 +1,19 @@
+package stringsAssignment;
+
+public class formTheBiggestNumber {
+
+	Comparator<String> {
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+           return order2.compareTo(order1);
+        }
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static int returnLargest(String a, String b) {
+
+	}
+}
